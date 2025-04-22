8892fdcde36b9e1cbf12adc1975117cd2188b9e5
zhshimanto <md.zakir5737@gmail.com>
2025-04-22 22:54:25 +0600

    free credit page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,40 +2,87 @@
   <div class="pt-[72px] flex-grow">
     <section class="px-5 md:px-0 py-12 mb-12">
       <div class="mx-auto max-w-6xl">
-        <h1 class="text-4xl md:text-5xl font-bold mb-8 text-center text-white">
-          ติดต่อ
+        <h1 class="text-2xl md:text-3xl font-bold mb-8 text-center text-white">
+          ติดต่อเรามัดรวมช่องทางติดต่อ PG โดยตรงที่เร็วที่สุด
         </h1>
         
-        <div class="max-w-3xl mx-auto bg-[#ff80b7]/80 backdrop-blur-sm rounded-xl p-6 md:p-8 shadow-xl border border-white/10">
+        <div class="max-w-3xl mx-auto bg-[#ff80b7]/80 backdrop-blur-sm rounded-xl p-6 md:p-8 shadow-xl border border-white/10 mb-8">
           <p class="text-white/90 text-lg md:text-xl text-center mb-8 leading-relaxed">
-            หากคุณมีปัญหาการใช้งาน สามารถดูวิธีการแก้ปัญหาเบื้องต้นได้ที่หน้า คำถามที่พบบ่อย FAQ หรือ ติดต่อ เจ้าหน้าที่ของ PG SLOT VEGAS ผ่านทาง LINE @902foaak, เรายินดีให้บริการ และ ช่วยตอบปัญหาของท่าน ตลอด 24 ช.ม.
+            ถ้าต้องการให้ช่วยเหลือหรือแก้ปัญหาต่าง ๆ ที่พบสามารถติดต่อเราได้เลยโดยตรงตามช่องทางที่ให้ไว้ ไม่ว่าจะเป็นปัญหาการฝากถอนเครดิต, ลืมรหัสผ่าน, เข้าเกมไม่ได้ หรืออยากพูดคุยในกลุ่มเล่นสล็อตว่าเกมไหนน่าเล่นเราก็พร้อมมีกลุ่มไลน์สล็อต pg ให้พูดคุยกันตลอด 24 ชั่วโมง
           </p>
+
+          <h2 class="text-2xl md:text-2xl font-bold mb-8  max-w-2xl mx-auto text-center text-white">ช่องทางติดต่อเรา PG TO</h2>
           
-          <a href="#" 
-             target="_blank" 
-             class="flex items-center justify-center bg-[#00B900] hover:bg-[#00A000] text-white text-lg font-medium px-8 py-4 rounded-lg mx-auto w-fit transition-all duration-300 hover:shadow-lg hover:-translate-y-0.5">
-            <img src="/images/line-icon.webp" alt="Line" class="w-6 h-6 mr-3" />
-            <span>ติดต่อทาง LINE</span>
+          <div class="flex flex-col md:flex-row justify-center items-center gap-4 w-full">
+            <a href="https://line.me/R/ti/p/@148amrcd" 
+               target="_blank" 
+               class="bg-[#00B900] hover:bg-[#00A000] text-white text-lg font-medium px-8 py-4 rounded-lg transition-all duration-300 hover:shadow-lg hover:-translate-y-0.5 flex-1 text-center min-w-[200px]">
+               LINE 1
+            </a>
+            <a href="https://line.me/R/ti/p/@541aiypv" 
+               target="_blank" 
+               class="bg-[#00B900] hover:bg-[#00A000] text-white text-lg font-medium px-8 py-4 rounded-lg transition-all duration-300 hover:shadow-lg hover:-translate-y-0.5 flex-1 text-center min-w-[200px]">
+               LINE 2
+            </a>
+            <a href="https://line.me/R/ti/p/@431tvrgg" 
+               target="_blank" 
+               class="bg-[#00B900] hover:bg-[#00A000] text-white text-lg font-medium px-8 py-4 rounded-lg transition-all duration-300 hover:shadow-lg hover:-translate-y-0.5 flex-1 text-center min-w-[200px]">
+               LINE 3
+            </a>
+          </div>
+          <div class="flex justify-center items-center space-x-6 mt-8">
+          <a href="https://www.facebook.com/playpg.to" target="_blank" rel="noopener noreferrer" class="text-white hover:text-pink-500 transition-colors duration-300">
+            <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+              <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
+            </svg>
+          </a>
+          <a href="https://x.com/playpgto" target="_blank" rel="noopener noreferrer" class="text-white hover:text-pink-500 transition-colors duration-300">
+              <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+              </svg>
           </a>
         </div>
+        </div>
       </div>
+      <div class="mx-auto max-w-6xl">
+        <h2 class="text-2xl md:text-2xl font-bold mb-8  max-w-2xl mx-auto  text-white">Contact PG Slot พัฒนา แก้ไข ช่วยเหลือ 24</h2>
+        <p class="text-white/90 text-lg max-w-2xl mx-auto md:text-xl mb-8 leading-relaxed">
+          เราคือผู้ให้บริการเกม PG SLOT ลิขสิทธิ์แท้ที่ต้องการพัฒนาแก้ไขและช่วยเหลือผู้ใช้บริการอย่างเต็มรูปแบบในทุก ๆ 24 ชั่วโมง ไม่ว่าจะเจอปัญหาไหนมาหรือต้องการเข้าถึงข้อมูลในด้านไหนเราก็พร้อมเข้าถึงปัญหาทุกรูปแบบอย่างมีประสิทธิภาพ
+        </p>
+        <p class="text-white/90 text-lg max-w-2xl mx-auto md:text-xl mb-8 leading-relaxed">
+          ลืมรหัสผ่าน
+          สมัครยังไง
+          ทำรายการถอนแล้วแต่เครดิตยังไม่เข้า
+          วิธีเล่นและเงื่อนไขเกมสล็อตมีอะไรบ้าง
+        </p>
+        <p class="text-white/90 text-lg max-w-2xl mx-auto md:text-xl mb-8 leading-relaxed">
+          บริการประทับใจติดต่อเรา 24 ชม.
+        </p>
+        <p class="text-white/90 text-lg max-w-2xl mx-auto md:text-xl mb-8 leading-relaxed">
+          ปัญหาทุกอย่างคำถามทุกข้อที่ต้องการไขเราพร้อมเป็นกุญแจทอดไปสู่คำตอบที่ตอบโจทย์แม่นยำและเชื่อถือได้ที่สุด เพราะเราคือเว็บบริการเกมสล็อตลิขสิทธิ์แท้ที่มีประสบการณ์ในการแก้ปัญหาได้ดีที่สุดในตอนนี้ ต้องการติดต่อกับเว็บ PG Slot ที่เร็วและสะดวกที่สุดในด้านไหนก็เลือกช่องทางติดต่อที่ให้ไว้ด้านบนได้เลย เราพร้อมดูแลคุณตลอดอายุยูสเซอร์ 24 ชั่วโมง
+        </p>
+      </div>
+      <div class="mx-auto max-w-6xl">
+        
+      </div>
+
     </section>
   </div>
 </template>
 
 <script setup>
 useHead({
-  title: 'ติดต่อ - PG SLOT VEGAS',
+  title: 'ติดต่อเรารวมช่องทางติดต่อเว็บตรง PGSLOT.TO',
   meta: [
-    { name: 'description', content: 'ติดต่อ PG SLOT VEGAS - ให้บริการและช่วยตอบปัญหาของท่านตลอด 24 ชั่วโมง ผ่านทาง LINE' },
-    { name: 'keywords', content: 'ติดต่อ PG SLOT VEGAS, LINE PG SLOT VEGAS, ช่วยเหลือ PG SLOT VEGAS, FAQ PG SLOT VEGAS' },
-    { property: 'og:title', content: 'ติดต่อ - PG SLOT VEGAS' },
-    { property: 'og:description', content: 'ติดต่อ PG SLOT VEGAS - ให้บริการและช่วยตอบปัญหาของท่านตลอด 24 ชั่วโมง ผ่านทาง LINE' },
+    { name: 'description', content: 'ติดต่อเราได้สะดวกลัดสั้นตรงและตอบโจทย์ทุกปัญหาคาใจได้ตลอด 24 ชั่วโมง เราพร้อมมีแอดมินชำนาญการพร้อมแก้ปัญหาทุกรูปแบบติดต่อ PG Slot โดยตรงกับเราได้เลยตอนนี้' },
+    { name: 'keywords', content: 'ติดต่อ PG Slot,กลุ่มไลน์ สล็อต pg,Contact PG Slot' },
+    { property: 'og:title', content: 'ติดต่อเรารวมช่องทางติดต่อเว็บตรง PGSLOT.TO' },
+    { property: 'og:description', content: 'ติดต่อเราได้สะดวกลัดสั้นตรงและตอบโจทย์ทุกปัญหาคาใจได้ตลอด 24 ชั่วโมง เราพร้อมมีแอดมินชำนาญการพร้อมแก้ปัญหาทุกรูปแบบติดต่อ PG Slot โดยตรงกับเราได้เลยตอนนี้' },
     { property: 'og:type', content: 'website' },
     { property: 'og:url', content: 'https://pg-vegas.com/contact' },
     { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'ติดต่อ - PG SLOT VEGAS' },
-    { name: 'twitter:description', content: 'ติดต่อ PG SLOT VEGAS - ให้บริการและช่วยตอบปัญหาของท่านตลอด 24 ชั่วโมง ผ่านทาง LINE' }
+    { name: 'twitter:title', content: 'ติดต่อเรารวมช่องทางติดต่อเว็บตรง PGSLOT.TO' },
+    { name: 'twitter:description', content: 'ติดต่อเราได้สะดวกลัดสั้นตรงและตอบโจทย์ทุกปัญหาคาใจได้ตลอด 24 ชั่วโมง เราพร้อมมีแอดมินชำนาญการพร้อมแก้ปัญหาทุกรูปแบบติดต่อ PG Slot โดยตรงกับเราได้เลยตอนนี้' }
   ]
 })
 </script>
