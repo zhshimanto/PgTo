773635cf15652f2dc6bd1622f770b940d3b6df2c
zhshimanto <md.zakir5737@gmail.com>
2025-04-23 12:48:37 +0600

    Analytics Code add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2023-11-01',
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: 'Tc61i_bmez5sVQDwU43aWJVrFLdjjXhobuQuV6rNwX8'
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXR8H6B9" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }
+      ],
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtm.js?id=GTM-TXR8H6B9',
+          async: true
+        },
+        {
+          key: 'gtm-init',
+          innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TXR8H6B9');
+          `,
+          type: 'text/javascript'
+        }
+      ]
+    }
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss'
