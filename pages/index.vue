634633f53916d6a1490c9e788b9a4968fbf319df
zhshimanto <md.zakir5737@gmail.com>
2025-06-03 01:51:29 +0600

    homepage content update

--- pages/index.vue
+++ pages/index.vue
@@ -13,17 +13,17 @@
 useHead({
 htmlAttrs: {
     lang: 'th'},
-  title: 'PG SLOT TO เว็บลิขสิทธิ์แท้โบนัสแจ็กพอตแตกง่ายที่สุด 2025',
+  title: 'PG SLOT TO เว็บตรงไม่ผ่านเอเย่นต์ ลิขสิทธิ์แท้จาก PGSLOT สล็อตแตกง่ายอันดับ 1 แห่งปี',
   meta: [
-    { name: 'description', content: 'PGSLOT.TO เว็บใหม่มาแรงที่สุดพร้อมให้บริการเกมลิขสิทธิ์แท้ส่งตรงจาก POCKET GAMES SOFT พีจีสล็อตสีชมพูแจ็กพอตแตกบ่อยฝากถอนไวจ่ายจริงผ่านระบบ API แท้ไม่ผ่านเอเยนต์' },
+    { name: 'description', content: 'PGSLOT.TO สล็อตเว็บตรงลิขสิทธิ์แท้จาก PGSOFT แจ็กพอตแตกง่าย ฝากถอนผ่านระบบ API แท้ จ่ายจริง ไม่ผ่านเอเยนต์ เล่นได้ 24 ชม.' },
     { name: 'keywords', content: 'PGSLOT, PG SLOT, PGSLOT TO, PG สีชมพู, สล็อตสีชมพู' },
-    { property: 'og:title', content: 'PG SLOT TO เว็บลิขสิทธิ์แท้พีจีสล็อตโบนัสแจ็กพอตแตกง่ายที่สุด 2025' },
-    { property: 'og:description', content: 'PGSLOT.TO เว็บใหม่มาแรงที่สุดพร้อมให้บริการเกมลิขสิทธิ์แท้ส่งตรงจาก POCKET GAMES SOFT พีจีสล็อตสีชมพูแจ็กพอตแตกบ่อยฝากถอนไวจ่ายจริงผ่านระบบ API แท้ไม่ผ่านเอเยนต์' },
+    { property: 'og:title', content: 'PG SLOT TO เว็บตรงไม่ผ่านเอเย่นต์ ลิขสิทธิ์แท้จาก PGSLOT สล็อตแตกง่ายอันดับ 1 แห่งปี' },
+    { property: 'og:description', content: 'PGSLOT.TO สล็อตเว็บตรงลิขสิทธิ์แท้จาก PGSOFT แจ็กพอตแตกง่าย ฝากถอนผ่านระบบ API แท้ จ่ายจริง ไม่ผ่านเอเยนต์ เล่นได้ 24 ชม.' },
     { property: 'og:type', content: 'website' },
     { property: 'og:url', content: 'https://pgslott.to' },
     { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: 'PG SLOT TO เว็บลิขสิทธิ์แท้พีจีสล็อตโบนัสแจ็กพอตแตกง่ายที่สุด 2025' },
-    { name: 'twitter:description', content: 'PGSLOT.TO เว็บใหม่มาแรงที่สุดพร้อมให้บริการเกมลิขสิทธิ์แท้ส่งตรงจาก POCKET GAMES SOFT พีจีสล็อตสีชมพูแจ็กพอตแตกบ่อยฝากถอนไวจ่ายจริงผ่านระบบ API แท้ไม่ผ่านเอเยนต์' }
+    { name: 'twitter:title', content: 'PG SLOT TO เว็บตรงไม่ผ่านเอเย่นต์ ลิขสิทธิ์แท้จาก PGSLOT สล็อตแตกง่ายอันดับ 1 แห่งปี' },
+    { name: 'twitter:description', content: 'PGSLOT.TO สล็อตเว็บตรงลิขสิทธิ์แท้จาก PGSOFT แจ็กพอตแตกง่าย ฝากถอนผ่านระบบ API แท้ จ่ายจริง ไม่ผ่านเอเยนต์ เล่นได้ 24 ชม.' }
   ]
 })
 
