788d66a5f65fd6952c53f4f7518f01801db9382d
zhshimanto <md.zakir5737@gmail.com>
2025-10-21 14:23:02 +0600

    GPTBot

--- server/routes/robots.txt.ts
+++ server/routes/robots.txt.ts
@@ -1,7 +1,8 @@
 export default defineEventHandler((event) => {
   const robotsConfig = `User-agent: *
 Allow: /
-
+User-agent: GPTBot
+Allow: /
 Sitemap: https://pgslot-to.to/sitemap.xml`
 
   event.node.res.setHeader('Content-Type', 'text/plain')
