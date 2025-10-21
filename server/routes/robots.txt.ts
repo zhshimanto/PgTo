52e1f62a03c9e107d7738cbb8ead8ed75873ffa8
zhshimanto <md.zakir5737@gmail.com>
2025-10-21 15:25:15 +0600

    chatgpt bot allow

--- server/routes/robots.txt.ts
+++ server/routes/robots.txt.ts
@@ -1,8 +1,13 @@
 export default defineEventHandler((event) => {
   const robotsConfig = `User-agent: *
 Allow: /
+
 User-agent: GPTBot
 Allow: /
+
+User-agent: Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko); compatible; GPTBot/1.1; +https://openai.com/gptbot
+Allow: /
+
 Sitemap: https://pgslot-to.to/sitemap.xml`
 
   event.node.res.setHeader('Content-Type', 'text/plain')
